8d08aea3069866d632f36dcfea824d1716a95ba9
readwanmd <readwanmd@gmail.com>
2022-01-24 01:52:45 +0600

    hero section done

--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ menuBtn.onclick = () => {
 closeNavbar.onclick = () => {
 	navbar.classList.remove('active');
 };
+
+let countDate = new Date('jan 31, 2022 00:00:00').getTime();
+
+function countDown() {
+	let now = new Date().getTime();
+
+	let gap = countDate - now;
+
+	let second = 1000;
+	let minute = second * 60;
+	let hour = minute * 60;
+	let day = hour * 24;
+
+	let d = Math.floor(gap / day);
+	let h = Math.floor((gap % day) / hour);
+	let m = Math.floor((gap % hour) / minute);
+	let s = Math.floor((gap % minute) / second);
+
+	document.getElementById('day').innerText = d;
+	document.getElementById('hour').innerText = h;
+	document.getElementById('minute').innerText = m;
+	document.getElementById('second').innerText = s;
+}
+
+setInterval(function () {
+	countDown();
+}, 1000);
